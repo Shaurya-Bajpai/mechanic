ec8767508a9ed1e352c780f1d0935dbcbf93d416
Shaurya Bajpai <shauryabajpai980@gmail.com>
2026-09-02 14:37:27 +0530

    Add MechanicApi interface and Retrofit instance

--- app/src/main/java/com/example/mechanic/data/remote/MechanicApi.kt
+++ app/src/main/java/com/example/mechanic/data/remote/MechanicApi.kt
@@ -1,2 +1,10 @@
 package com.example.mechanic.data.remote
 
+import com.example.mechanic.data.model.Mechanic
+import retrofit2.http.GET
+
+interface MechanicApi {
+
+    @GET("mechanics")
+    suspend fun getMechanics(): List<Mechanic>
+}
--- app/src/main/java/com/example/mechanic/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/mechanic/data/remote/RetrofitInstance.kt
@@ -1,2 +1,16 @@
 package com.example.mechanic.data.remote
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = ""
+
+    val api: MechanicApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MechanicApi::class.java)
+    }
+}
